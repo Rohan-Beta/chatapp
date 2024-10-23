13ac4a4720f8b7e3e336fe0f39e900689dcc4f67
Rohan-Beta <rohitraha952@gmail.com>
2024-10-23 18:36:18 +0530

    feat: update AndroidManifest.xml file for permission

--- lib/controller/call_controller.dart
+++ lib/controller/call_controller.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import 'package:chatapp/model/audio_call_model.dart';
 import 'package:chatapp/model/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uuid/uuid.dart';
 
